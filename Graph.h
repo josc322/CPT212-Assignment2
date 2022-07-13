2e90719f8a7d52958809ed9dc73d9ae5a35b84e7
deftmemeda <96969953+josc322@users.noreply.github.com>
2022-07-13 21:33:23 +0800

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -11,8 +11,19 @@ using namespace std;
 /// Data structure to store a graph edge
 struct Edge {
     int src, dest, weight;
+    Edge(int start,  int end, int w) {
+        src = start;
+        dest = end;
+        weight = w;
+    }
 };
- 
+
+struct Node {
+	unordered_map<int, Edge*> edgesComingIn;
+    unordered_map<int, Edge*> edgesGoingOut;
+    Edge* edgeInMDST;
+};
+
 typedef pair<int, int> Pair;
 
 class Graph{
@@ -20,11 +31,16 @@ class Graph{
 public:
     // a vector of vectors of Pairs to represent an adjacency list
     vector<vector<Pair>> adjList;
+    vector<Node*> nodes;
     Graph(vector<Edge> &edges, int n);   // Constructor
     void addEdge(Graph &graph, int u, int v);   // to add an edge to graph
     bool edgeExists(Graph &graph, int u, int v); //check if edge exists
     bool isCyclic(Graph &graph);    // returns true if there is a cycle in this graph
     void displayGraph(Graph &graph);
+    void EdmondsAlgorithm(int n);
+	int findACycle(int vertex, unordered_map<int, Edge*>& vertexToMinEdge, unordered_set<int>& toRoot, unordered_set<int>& currentPath, unordered_set<Edge*>& cycleEdges, unordered_set<int>& cycleNodes);
+	void DFS(int n, unordered_set<int>& seen);
+	void MST(vector<vector<Pair>> adjList);
     ~Graph() { }
 };
 
