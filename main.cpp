e31706132292d83d251d17864789f7ca59c8852d
hanashah-01 <88876401+hanashah-01@users.noreply.github.com>
2022-07-13 18:01:10 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main()
 	// graph edges array.
     vector<Edge> edges = {
         // (x, y, w) -> edge from x to y with weight w
-        {0,4,10216.47},{1,2,488.05},{2,3,1526.20},{3,4,3770.79},{0,1,7016.11}
+        {0,4,10216},{1,2,488},{2,3,1526},{3,4,3771},{0,1,7016}
     };
     int vertices = 5;
     //Create graph
