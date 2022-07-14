3ed4d0771fcf7926c9f9459ecca84955e8e0c325
deftmemeda <96969953+josc322@users.noreply.github.com>
2022-07-14 19:47:46 +0800

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -11,17 +11,6 @@ using namespace std;
 /// Data structure to store a graph edge
 struct Edge {
     int src, dest, weight;
-    Edge(int start,  int end, int w) {
-        src = start;
-        dest = end;
-        weight = w;
-    }
-};
-
-struct Node {
-    unordered_map<int, Edge*> edgesComingIn;
-    unordered_map<int, Edge*> edgesGoingOut;
-    Edge* edgeInMDST;
 };
 
 typedef pair<int, int> Pair;
@@ -32,21 +21,17 @@ class Graph{
 public:
     // a vector of vectors of Pairs to represent an adjacency list
     vector<vector<Pair>> adjList;
-    vector<Node*> nodes;
     Graph(vector<Edge> &edges, int n);   // Constructor
     void addEdge(Graph &graph, int u, int v);   // to add an edge to graph
     bool edgeExists(Graph &graph, int u, int v); //check if edge exists
     bool hasCycle(Graph &graph, vector<int> &path);    // returns true if there is a cycle in this graph
     void displayGraph(Graph &graph);
-    void EdmondsAlgorithm(int n);
-    int findACycle(int vertex, unordered_map<int, Edge*>& vertexToMinEdge, unordered_set<int>& toRoot, unordered_set<int>& currentPath, unordered_set<Edge*>& cycleEdges, unordered_set<int>& cycleNodes);
-    void DFS(int n, unordered_set<int>& seen);
-    void MST(vector<vector<Pair>> adjList);
-    void countEdges(Graph const &graph);
-    void clearGraph(vector<vector<Pair>> adjList, int n);
-	void resetGraph(vector<vector<Pair>> adjList, vector<Edge> edges, int n);
+    void countEdges(Graph const &graph); // count the number of edges in the graph
+    void clearGraph(vector<vector<Pair>> adjList, int n); // clear the graph
+    void resetGraph(vector<vector<Pair>> adjList, vector<Edge> edges, int n); // reset the graph back to default graph
     bool isSC(Graph &graph); //returns true if there are strong connection between graph
     void getReverse(Graph &graph); //for bool isSC
+    void PrimsAlgorithm(int n, vector<vector<Pair>> adjList); // Find the MST of the graph
 	
 	
     ~Graph() { }
